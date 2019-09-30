05bc17371bf9ade543c548061b6adc0607c03d97
developer <developer@K-1312.SRGJ.local>
2019-09-30 09:05:28 +0200

    Ręczny 2

--- backup/u_parametry.pas
+++ backup/u_parametry.pas
@@ -383,7 +383,8 @@ begin
   FOperacje.LNazwa.Font.Color := skib_InvertColor(FOperacje.Color);
   if Ramka <> nil then begin
     Ramka.UstawKolorObramowania(FOperacje.Color);
-    Ramka.Brush.Color := Ramka.Pen.Colorxxxxxyyyy; //na potrzeby WybierzObrazek - zmiana tla Ramki - 2019.09.29
+    Ramka.Brush.Color := Ramka.Pen.Color; //na potrzeby WybierzObrazek - zmiana tla Ramki - 2019.09.29
+    //git git git
   end;
   for i := 1 to TMojImage.liczbaOb do
     if FOperacje.tabOb[i].JestLapka then
